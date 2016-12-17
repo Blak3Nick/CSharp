82d3d7aacfc3181dffa1739ce4abbbf54cfe6fa2
Blak3Nick <blaken2021@gmail.com>
2016-12-17 15:26:28 -0500

    added new files

---
+++ BasicPawn.cs
@@ -0,0 +1,11 @@
+namespace CardGameBlake
+{
+	class BasicPawn
+    {
+        readonly string Description = "Basic unit with low attack and no special skills";
+        public string ReturnPawnDesc()
+        {
+            return Description;
+        }
+    }
+}
---
+++ CardGame.cs
@@ -0,0 +1,14 @@
+﻿namespace CardGameBlake
+{
+	 class CardGame
+	{
+		public static void Main()
+		{
+			BasicPawn pawn = new BasicPawn();
+			System.Console.WriteLine ("Hello world");
+			// string returned = pawn.ReturnPawnDesc();
+			// System.Console.WriteLine (returned);
+		}
+	}
+}
+
