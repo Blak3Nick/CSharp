cdd2e4f9872a8ec3c1c1581de3c2dc44e300eb43
Blak3Nick <blaken2021@gmail.com>
2016-12-18 17:04:16 -0500

    new method

--- MapLocation.cs
+++ MapLocation.cs
@@ -2,9 +2,12 @@ namespace CardGameBlake
 {
     class MapLocation
     {
+        int xLocation;
+        int yLocation;
         public MapLocation(int x, int y)
         {
-            
+            this.xLocation = x;
+            this.yLocation = y;
         }
     }
 }
