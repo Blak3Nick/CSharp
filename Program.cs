2384c98616e4d38ae573452b502c1696528072e7
Blak3Nick <blaken2021@gmail.com>
2016-12-17 20:46:41 -0500

    added new methods

--- Program.cs
+++ Program.cs
@@ -9,20 +9,30 @@ namespace CardGameBlake
 			CardPool player1CardPool = new CardPool();
 			CardPool player2CardPool = new CardPool();
 			ConvertString convert = new ConvertString();
+
             Console.WriteLine("PLayer 1, How many pawns would you like?");
 			string PawnSelection = Console.ReadLine();
 			int intPawnSelection = convert.Convert(PawnSelection);
 			GameLaunch game = new GameLaunch(7,7);
-			
 			player1CardPool.setNumOfPawns(intPawnSelection);
 			int pawns = player1CardPool.getNumOfPawns();
 			Console.WriteLine("Your number of pawns is "+ pawns);
+
 			Console.WriteLine("PLayer 1, How many Ice Wizards would you like?");
 			string IceWizardSelection = Console.ReadLine();
 			int intIceWizardSelection = convert.Convert(IceWizardSelection);
 			player1CardPool.setNumOfIceWizards(intIceWizardSelection);
 			int icewizards = player1CardPool.getNumOfIceWizards();
 			Console.WriteLine("Your number of Ice Wizards is "+ icewizards);
+
+			Console.WriteLine("PLayer 1, How many Giants would you like?");
+			string GiantSelection = Console.ReadLine();
+			int intGiantSelection = convert.Convert(GiantSelection);
+			player1CardPool.setNumOfGiants(intGiantSelection);
+			int giants = player1CardPool.getNumOfGiants();
+			Console.WriteLine("Your number of Giants is "+ giants);
+
+
 
 		}
 	}
