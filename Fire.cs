f4523c81cf14afdfb64b7bef16a16f42018f50c5
Blak3Nick <blaken2021@gmail.com>
2016-12-19 17:22:41 -0500

    Introduced new Fire status

---
+++ Fire.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGameBlake
+{
+    public class Fire
+    {
+        public bool fireAttackSuccess= false;
+        public bool fireStatus = false;
+        public bool FireAttack(double defendingUnitMagicDefense, double attackingUnitFireRating)
+        {
+            //Fire attack success should increase damage dealt by FireAttackPower and increase exponentially if successful through fire status
+            if (defendingUnitMagicDefense > attackingUnitFireRating)
+            {
+                fireAttackSuccess = false;
+            }
+            else
+            { fireAttackSuccess = true;
+            }
+            return fireAttackSuccess;         
+        }
+        public bool FireStatus(bool fireAttackSuccess)
+        {
+            if(fireAttackSuccess)
+            {
+                fireStatus = true;
+            }
+            else { fireStatus = false; }
+            return fireStatus;
+        }
+    }
+}
